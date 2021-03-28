df826d1cec7b153d316371c54f9ba0c09ac3f30e
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2021-03-28 13:25:52 +0600

    constraint

--- app/src/main/java/kz/example/activitylesson/FirstActivity.kt
+++ app/src/main/java/kz/example/activitylesson/FirstActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class FirstActivity : AppCompatActivity() {
 
@@ -14,14 +17,36 @@ class FirstActivity : AppCompatActivity() {
         setContentView(R.layout.activity_first)
 
         val button = findViewById<Button>(R.id.btnNextActivity)
+        val btnShowSnackbar = findViewById<Button>(R.id.btnShowSnackbar)
+        val editText = findViewById<EditText>(R.id.etName)
 
-        button.setOnClickListener (object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val intent = Intent(this@FirstActivity.applicationContext, FourActivity::class.java)
-                startActivity(intent)
-            }
+//        button.setOnClickListener (object : View.OnClickListener {
+//            override fun onClick(v: View?) {
+////                val intent = Intent(this@FirstActivity.applicationContext, FourActivity::class.java)
+////                startActivity(intent)
+//                Toast.makeText(this@FirstActivity, "Some text", Toast.LENGTH_LONG)
+//                    .show()
+//            }
+//
+//        })
 
-        })
+        button.setOnClickListener {
+            Toast.makeText(this@FirstActivity, "You write: ${editText.text}", Toast.LENGTH_LONG)
+                .show()
+        }
+
+        btnShowSnackbar.setOnClickListener {
+            val snackbar = Snackbar.make(
+                btnShowSnackbar,
+                "You write: ${editText.text}",
+                Snackbar.LENGTH_INDEFINITE
+            )
+            snackbar.setAction("Понял", {
+                snackbar.dismiss()
+            })
+            snackbar.show()
+
+        }
     }
 
 
